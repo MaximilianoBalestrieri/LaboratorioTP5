a535a678afece5b126c13b3e3c066b2ce82e950a
Maximiliano Balestrieri <maximiliano_balestrieri@hotmail.com>
2024-04-10 20:54:25 -0300

    imagenes

---
+++ src/Entidades/Producto.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author CCMEW
+ */
+public class Producto {
+    private String categoria;
+    private double precio;
+    private String descripcion;
+
+    public Producto(String categoria, double precio, String descripcion) {
+        this.categoria = categoria;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
